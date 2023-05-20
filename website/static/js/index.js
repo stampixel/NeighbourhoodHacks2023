118ae4a4cc68756a468360ca26376867a9b1acef
himanshukumar04 <himanshu.kumar04@nagarro.com>
2023-05-20 20:43:44 +0530

    editor page

---
+++ website/static/js/index.js
@@ -0,0 +1,11 @@
+function openGoogleMaps() {
+  window.location.href = "https://maps.google.com";
+}
+
+function navigateToLink(url) {
+  window.location.href = url;
+}
+
+function subscription() {
+  // noop functionality right now
+}
